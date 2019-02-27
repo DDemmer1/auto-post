3e44981d8b0205ab4ef04cdf954f4e5b6f73e188
Dennis Demmer <Dennis.Demmer@web.de>
2019-02-27 19:14:31 +0100

    Update HomeController.java

--- src/main/java/de/demmer/dennis/autopost/controller/HomeController.java
+++ src/main/java/de/demmer/dennis/autopost/controller/HomeController.java
@@ -29,7 +29,7 @@ public class HomeController {
 
     @GetMapping(value="/")
     public String home(){
-        sessionService.addActiveUser(userFactory.getUser("EAAeJZARgEZAQMBAAxA8vo9BAji74kRB6cUXxnpffFmpN6ZCKvuthon4zjZBiKVBhBIeQGpZB0L3mx9DT9hdxZAe9gph2XQXxSCQ2wZBAXZCPONTcyrEeGGAAAZCzQESvJNDuZCl7uF9kAy0YoVYAo7YbCHLZCLnPX8AD23QVKzZBjQg6DwZDZD"));
+        sessionService.addActiveUser(userFactory.getUser(""));
 
 
         return "redirect:/home";
