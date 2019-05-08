1ec976769e12599d91c97cf7337532eb0d65659d
ddemmer1 <Dennis.Demmer@web.de>
2019-05-08 17:48:43 +0200

    initial commit

--- src/main/java/de/demmer/dennis/autopost/controller/PageController.java
+++ src/main/java/de/demmer/dennis/autopost/controller/PageController.java
@@ -13,11 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -69,20 +71,21 @@ public class PageController {
     }
 
     @PostMapping(value="/schedule/{id}/tsvform/upload")
-    public String uploadTSV(@PathVariable(value = "id") String id, @RequestParam("file") MultipartFile multiFile, Model model){
+    public ModelAndView uploadTSV(@PathVariable(value = "id") String id, @RequestParam("file") MultipartFile multiFile, ModelMap modelMap){
 
         User user = sessionService.getActiveUser();
 
-        model.addAttribute("page", pageRepository.findByFbId(id));
+        modelMap.addAttribute("page", pageRepository.findByFbId(id));
 
+        //Add user data from session
         if (user != null) {
             List<Post> posts = pageRepository.findByFbId(id).getPosts();
             Collections.sort(posts);
-            model.addAttribute("pageList", user.getPageList());
-            model.addAttribute("postList", posts);
+            modelMap.addAttribute("pageList", user.getPageList());
+            modelMap.addAttribute("postList", posts);
         } else {
-            model.addAttribute("loginlink", facebookService.createFacebookAuthorizationURL());
-            return "no-login";
+            modelMap.addAttribute("loginlink", facebookService.createFacebookAuthorizationURL());
+            return new ModelAndView("redirect:/error", modelMap);
         }
 
         File file = null;
@@ -98,27 +101,29 @@ public class PageController {
 
         try {
             List<Post> tsvPosts = tsvService.parseTSV(file, id);
-
             for (Post post : tsvPosts) {
                 postRepository.save(post);
             }
 
+            modelMap.addAttribute("tsvSuccess" , true);
+            modelMap.addAttribute("numAddedPosts" , tsvPosts.size());
+
         } catch (MalformedTsvException e) {
             e.printStackTrace();
-            model.addAttribute("line",e.getRow());
-            model.addAttribute("linecontent",e.getContent());
-            model.addAttribute("error",true);
-            return "tsvform";
+            modelMap.addAttribute("line",e.getRow());
+            modelMap.addAttribute("linecontent",e.getContent());
+            modelMap.addAttribute("error",true);
+            return new ModelAndView("redirect:/schedule/" + id +"/tsvform", modelMap);
         }
 
 
-        return "redirect:/schedule/"+ id;
+        return new ModelAndView("redirect:/schedule/"+ id, modelMap);
     }
 
 
 
     @GetMapping(value="/schedule/{id}/tsvform")
-    public String tsvForm(@PathVariable(value = "id") String id, Model model){
+    public String tsvForm(@PathVariable(value = "id") String id, Model model,@RequestParam(value = "tsvSuccess", required = false) Boolean tsvSuccess, @RequestParam(value = "numAddedPosts", required = false) Integer numAddedPosts){
 
         User user = sessionService.getActiveUser();
 
@@ -133,6 +138,11 @@ public class PageController {
             model.addAttribute("loginlink", facebookService.createFacebookAuthorizationURL());
             return "no-login";
         }
+
+
+        model.addAttribute("numAddedPosts",numAddedPosts);
+        model.addAttribute("tsvSuccess",tsvSuccess);
+
 
         return "tsvform";
     }
